f1a39b945e0c708fe8a31a099bc20567995d0971
Ugur <ugurkeklik.eem@gmail.com>
2022-05-03 21:58:42 +0100

    edit more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello 2");
+        System.out.println("Hello 3");
     }
 
 }
